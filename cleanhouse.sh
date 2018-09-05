680b77860c46a587d971606901d14da82a667f0d
Kevin McCormack <kem2028@med.cornell.edu>
2018-09-05 13:20:33 -0400

    Update cleanhouse

--- cleanhouse.sh
+++ cleanhouse.sh
@@ -13,6 +13,9 @@ if command -v apt >/dev/null 2>&1; then
   printf "Running apt autoremove and autoclean...\n"
   sudo apt -y autoremove
   sudo apt -y autoclean
+  sudo apt -y clean
+  printf "Removing residual config files...\n"
+  sudo dpkg --purge $(dpkg --get-selections | grep deinstall | cut -f1)
 elif command -v pacman >/dev/null 2>&1; then
   printf "Cleaning up Pacman cache...\n"
   sudo paccache -r
@@ -33,6 +36,9 @@ find ~/. -type l -! -exec test -e {} \; -delete
 
 printf "Removing all but the last 30 days of journal logs...\n"
 sudo journalctl --vacuum-time=30d
+
+printf "Taking out the trash...\n"
+trash-empty
 
 printf "--After--\n"
 df -h --total
