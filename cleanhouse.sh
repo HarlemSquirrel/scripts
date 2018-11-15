b9664f7997b3a109fa77818802e30aed6fd3a9ff
Kevin McCormack <kem2028@med.cornell.edu>
2018-11-15 12:28:29 -0500

    Update cleanhouse.sh
    - Colorize some output
    - Add some whitespace between cleanup commands
    - Add some comments

--- cleanhouse.sh
+++ cleanhouse.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-printf "  --Before--\n"
+# Load helper functions
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+source $SCRIPTPATH/helper_functions.sh
+
+# Show disk usage before
+colorprintf blue "\n  --Before--\n"
 df -h --total
 
 printf "\nWe are now going to clean some shit up...\n"
@@ -10,14 +16,14 @@ printf "\nWe are now going to clean some shit up...\n"
 # sudo find /var/* -atime +30 -delete
 
 if command -v apt >/dev/null 2>&1; then
-  printf "Running apt autoremove and autoclean...\n"
+  colorprintf orange "\nRunning apt autoremove and autoclean...\n"
   sudo apt -y autoremove
   sudo apt -y autoclean
   sudo apt -y clean
-  printf "Removing residual config files...\n"
+  colorprintf orange "\nRemoving residual config files...\n"
   sudo dpkg --purge $(dpkg --get-selections | grep deinstall | cut -f1)
 elif command -v pacman >/dev/null 2>&1; then
-  printf "Cleaning up Pacman cache...\n"
+  colorprintf orange "\nCleaning up Pacman cache...\n"
   sudo paccache -r
   sudo paccache -ruk0
 fi
@@ -30,17 +36,18 @@ fi
 # find ~/.local/share/* -atime +90 | wc -l
 # find ~/.local/share/* -atime +90 -delete
 
-printf "Removing broken symlinks...\n"
+colorprintf orange "\nRemoving broken symlinks...\n"
 find ~/. -type l -! -exec test -e {} \; -print | wc -l
 find ~/. -type l -! -exec test -e {} \; -delete
 
-printf "Removing all but the last 30 days of journal logs...\n"
+colorprintf orange "\nRemoving all but the last 30 days of journal logs...\n"
 sudo journalctl --vacuum-time=30d
 
-printf "Taking out the trash...\n"
+colorprintf orange "\nTaking out the trash...\n"
 trash-empty
 
-printf "  --After--\n"
+# Show disk usage after
+colorprintf blue "\n  --After--\n"
 df -h --total
 
-printf "\nAll done here!\n"
+colorprintf green "\nAll done here!\n"
