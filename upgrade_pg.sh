ea2023ca835d7dc40299c8d40f138da1e4f01ff8
Kevin McCormack <harlemsquirrel@gmail.com>
2018-11-19 22:34:18 -0500

    Update upgrade_pg.sh comments

--- upgrade_pg.sh
+++ upgrade_pg.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+## upgrade_pg.sh
+# A friendly script for upgrading PostgreSQL on ArchLinux
+# Adapted from https://wiki.archlinux.org/index.php/PostgreSQL#Upgrading_PostgreSQL
+
 # Make sure only root can run our script
 if [[ $EUID -ne 0 ]]; then
    echo "  $0 must be run as root!" 1>&2
