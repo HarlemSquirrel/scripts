4f98eeda1bc96d09c53b8b275440c32887eca194
Kevin McCormack <harlemsquirrel@gmail.com>
2019-01-05 16:36:38 -0500

    Update helper_functions

--- helper_functions.sh
+++ helper_functions.sh
@@ -1,6 +1,7 @@
 # Function to print in specified color
 colorprintf () {
 	case $1 in
+		"black") tput setaf 0;;
 		"red") tput setaf 1;;
 		"green") tput setaf 2;;
 		"orange") tput setaf 3;;
@@ -9,6 +10,7 @@ colorprintf () {
 		"cyan") tput setaf 6;;
 		"gray" | "grey") tput setaf 7;;
 		"white") tput setaf 8;;
+		"default") tput setaf 9;;
 	esac
 	printf "$2";
 	tput sgr0
