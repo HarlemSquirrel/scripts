946ff0ff2f35226a8de49f86d2db3bc7ef078a38
Kevin McCormack <harlemsquirrel@gmail.com>
2019-01-06 16:56:09 -0500

    Update rpi-check-throttling

--- rpi-check-throttling.py
+++ rpi-check-throttling.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python2
 
+# https://github.com/raspberrypi/firmware/commit/404dfef3b364b4533f70659eafdcefa3b68cd7ae#commitcomment-31620480
+# 1110000000000000010
+# |||             |||_ under-voltage
+# |||             ||_ currently throttled
+# |||             |_ arm frequency capped
+# |||_ under-voltage has occurred since last reboot
+# ||_ throttling has occurred since last reboot
+# |_ arm frequency capped has occurred since last reboot
+
 import subprocess
 
 GET_THROTTLED_CMD = 'vcgencmd get_throttled'
@@ -12,6 +21,24 @@ MESSAGES = {
     18: 'ARM frequency capped has occurred since last reboot.'
 }
 
+class Logger:
+    COLOR_ERROR = '\033[91m'
+    COLOR_SUCCESS = '\033[92m'
+    COLOR_WARNING = '\033[93m'
+    COLOR_RESET = '\033[0m'
+
+    @classmethod
+    def error(cls, msg):
+        print(cls.COLOR_ERROR + msg + cls.COLOR_RESET)
+
+    @classmethod
+    def success(cls, msg):
+        print(cls.OLOR_SUCCESS + msg + cls.COLOR_RESET)
+
+    @classmethod
+    def warn(cls, msg):
+        print(cls.COLOR_WARNING + msg + cls.COLOR_RESET)
+
 print("Checking for throttling issues since last reboot...")
 
 throttled_output = subprocess.check_output(GET_THROTTLED_CMD, shell=True)
@@ -20,11 +47,11 @@ throttled_binary = bin(int(throttled_output.split('=')[1], 0))
 warnings = 0
 for position, message in MESSAGES.iteritems():
     # Check for the binary digits to be "on" for each warning message
-    if len(throttled_binary) > position and [0 - position - 1] == '1':
-        print(message)
+    if len(throttled_binary) > position and throttled_binary[0 - position - 1] == '1':
+        Logger.error(message)
         warnings += 1
 
 if warnings == 0:
-    print("Looking good!")
+    Logger.success("Looking good!")
 else:
-    print("Houston, we may have a problem!")
+    Logger.warn("Houston, we may have a problem!")
