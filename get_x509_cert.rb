aecdb8e53fafa146a4dceefaab52680e377353b6
Kevin McCormack <kem2028@med.cornell.edu>
2020-04-01 09:59:44 -0400

    Update get_x509_cert.rb

--- get_x509_cert.rb
+++ get_x509_cert.rb
@@ -1,4 +1,10 @@
 #!/usr/bin/env ruby
+##
+# Print out the certificate details for the provided host [port].
+# Also include the certificate in PEM format.
+#
+# Equivelant to
+# echo | openssl s_client -servername $host -connect $host:443 2>/dev/null | openssl x509 -text
 
 require 'net/http'
 
@@ -6,8 +12,11 @@ host = ARGV[0]
 port = (ARGV[1] || 443)
 cert = nil
 
-Net::HTTP.start(host, port, use_ssl: true) do |http|
+Net::HTTP.start(host, port, use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
   cert = http.peer_cert
 end
 
-puts cert.to_s
+puts cert.to_text
+
+puts "\n\nPEM format:",
+     cert.to_s
