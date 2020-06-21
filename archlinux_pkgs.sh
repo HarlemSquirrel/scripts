2296623eab9dfc32d60e3a03bc2bcf31a1c4bafc
Kevin McCormack <harlemsquirrel@mccormack.tech>
2020-06-21 19:12:30 -0400

    Update archlinux_pkgs

--- archlinux_pkgs.sh
+++ archlinux_pkgs.sh
@@ -4,6 +4,7 @@
 cli=(fzf zsh)
 inkscape_recommends=(pstoedit  python2-scour texlive-core python2-lxml uniconvertor)
 dev_tools=(atom ctags docker docker-compose git gnome-nettool nmap postgresql phantomjs qt5-webkit terminator vim yarn)
+fonts=(ttf-dejavu)
 gaming=(steam steam-native-runtime)
 general=(chromium deja-dup dropbox firefox)
 hp_printing=(hplip python-reportlab python-pyqt4 python-pyqt5 sane xsane)
@@ -19,6 +20,9 @@ sudo pacman -S ${cli[*]}
 
 printf 'Dev tools ...\n'
 sudo pacman -S ${dev_tools[*]}
+
+printf 'Fonts...\n'
+sudo pacman -S ${fonts[*]}
 
 printf 'Gaming...\n'
 sudo pacman -S ${gaming[*]}
