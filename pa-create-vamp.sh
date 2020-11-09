81fd7769aacff12c94be3b661b7a757175dc2939
Kevin McCormack <harlemsquirrel@mccormack.tech>
2020-11-09 18:47:01 -0500

    Add pa-create-vamp.sh

---
+++ pa-create-vamp.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+##
+# PulseAudio Virtual Audio Mapping Device
+#
+# Create a virtual output device that remaps to an input device.
+#
+# Point any combination of outputs from music player, OBS, etc. to the virtual output device
+# and then use the remapped source as the input for broadcasting, recording, etc.
+#
+
+set -e
+
+# Create the null (virtual) output device
+# https://www.freedesktop.org/wiki/Software/PulseAudio/Documentation/User/Modules/#module-null-sink
+pactl load-module module-null-sink \
+  sink_name=VAMP \
+  sink_properties=device.description=VAMP
+
+# Create the virtual input device
+# https://www.freedesktop.org/wiki/Software/PulseAudio/Documentation/User/Modules/#module-remap-source
+pactl load-module module-remap-source \
+  source_name=Remap-Source
+  master=VAMP.monitor
