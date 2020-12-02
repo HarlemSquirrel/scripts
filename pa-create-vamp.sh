2700f78bf9cb56cf895e1af4bf686058b9497893
Kevin McCormack <harlemsquirrel@mccormack.tech>
2020-12-01 23:17:17 -0500

    Update pa-create-vamp.sh

--- pa-create-vamp.sh
+++ pa-create-vamp.sh
@@ -11,6 +11,9 @@
 
 set -e
 
+# Exit early if the VAMP is already created.
+pactl list short | grep sink_name=VAMP && echo 'Already created' && exit
+
 # Create the null (virtual) output device
 # https://www.freedesktop.org/wiki/Software/PulseAudio/Documentation/User/Modules/#module-null-sink
 pactl load-module module-null-sink \
@@ -22,3 +25,7 @@ pactl load-module module-null-sink \
 pactl load-module module-remap-source \
   source_name=Remap-Source \
   master=VAMP.monitor
+
+# pactl list short | grep VAMP
+
+echo 'VAMP virtual output and input are ready to go!'
