92d9c805a9d5f200dc1f418ca39960a249a0f989
Kevin McCormack <harlemsquirrel@mccormack.tech>
2021-01-19 11:43:50 -0500

    Update dev install script

--- dev-env-setup.sh
+++ dev-env-setup.sh
@@ -30,6 +30,16 @@ fi
 printf "\n==> Installing/updating NVM...\n"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install node
+
+# Yarn
+# https://yarnpkg.com/getting-started/install
+npm install -g yarn
+
 # Oh My Zsh
 # https://github.com/ohmyzsh/ohmyzsh
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
