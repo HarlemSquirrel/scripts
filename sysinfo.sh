b45a1f732adb48d9a488ed3fbc61ad55362dd58a
Kevin McCormack <harlemsquirrel@mccormack.tech>
2021-10-06 14:01:33 -0400

    Update sysinfo.sh

--- sysinfo.sh
+++ sysinfo.sh
@@ -30,8 +30,11 @@ printf "\n\n**System Memory:\n"
 if [[ $EUID > 0 ]]; then
   printf "(Run as root to see more memory info)\n"
 else
+  printf "*From dmidecode:\n"
   dmidecode -t memory | grep -P '(Memory Device|\tBank Locator: \w+|\tSpeed: \d+|\tSize: \d+|Configured Voltage: [0-9]|Part Number:|\tManufacturer:)'
 fi
+printf "\n*From decode-dimms:\n"
+decode-dimms 2>>/dev/null | grep -P '(Guessing DIMM is in|Supported CAS Latencies|Size|Fundamental Memory type|CAS Latency)'
 
 gpu_name=`lspci -v -s $(lspci -v | grep VGA | cut -d ' ' -f 1) | grep -oP "(?<=\tSubsystem: ).+"`
 gpu_memory_total="$(($(cat /sys/class/drm/card0/device/mem_info_vram_total) / 2**20))"
