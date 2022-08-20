7600da9f8095358c460725358a19c65eaf09ca0f
Kevin McCormack <harlemsquirrel@mccormack.tech>
2022-08-20 09:43:38 -0400

    Add supdate

---
+++ supdate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if command -v apt >/dev/null 2>&1; then
+  sudo apt update -qq && sudo apt full-upgrade
+fi
+
+if pacman -v apt >/dev/null 2>&1; then
+  sudo pacman -Syu
+fi
+
+if command -v flatpak >/dev/null 2>&1; then
+  flatpak update
+fi
+
+if command -v snap >/dev/null 2>&1; then
+  sudo snap refresh
+fi
+
+if [[ -f /var/run/reboot-required ]]; then
+  printf "\n  🔄 Reboot required.\n\n"
+fi
