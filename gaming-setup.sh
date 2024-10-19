b4e33cd883a2c5478e9b4a6402344074373ab965
Kevin McCormack <kevin@mccormack.tech>
2024-10-19 12:33:28 -0400

    Add gaming-setup

---
+++ gaming-setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if command -v apt >>/dev/null; then
+  printf "\n==> Installing dependencies with apt...\n"
+
+  if ( ! command -v polychromatic-cli >>/dev/null ); then
+    # https://polychromatic.app/download/ubuntu/
+    sudo add-apt-repository ppa:openrazer/stable
+    sudo add-apt-repository ppa:polychromatic/stable
+    sudo apt install openrazer-meta polychromatic
+  fi
+
+  if ( ! command -v signal-desktop >>/dev/null ); then
+    # https://signal.org/download/linux/
+    # 1. Install our official public software signing key:
+    wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+    cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+
+    # 2. Add our repository to your list of repositories:
+    echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+    sudo tee /etc/apt/sources.list.d/signal-xenial.list
+
+    # 3. Update your package database and install Signal:
+    sudo apt update && sudo apt install signal-desktop
+  fi
+fi # end apt
+
+if command -v snap >>/dev/null; then
+  printf "\n==> Installing dependencies with snap...\n"
+
+  if ( ! command -v steam >>/dev/null ); then
+    snap install steam
+  fi
+fi
+
+printf "\nDone\n"
