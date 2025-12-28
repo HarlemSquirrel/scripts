66da60358ac497ff6dadfa06a703d827ff235930
Kevin McCormack <kevin@mccormack.tech>
2025-12-28 18:02:47 -0500

    Add docker_deb_update

---
+++ docker_deb_update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+docker compose down
+
+# Backup
+zip -r "frigate_$(date +'%Y%m%d.%H%M').zip" frigate
+rsync -a backups/* nas:/mnt/BigPool/backups/frigate/
+
+# Cleanup
+docker system prune -f
+
+# Update system
+sudo apt-get update
+sudo apt-get dist-upgrade -y
+
+# Update containers
+docker compose pull
+
+# Do containers will come up after reboot automatically
+docker compose up -d --remove-orphans --force-recreate
+
+# Reboot
+sudo reboot
