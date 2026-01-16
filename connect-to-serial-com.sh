654687f1ab1913eeaeb8c002d8dec6139022d43d
Kevin McCormack <kevin@mccormack.tech>
2026-01-15 19:29:26 -0500

    Add notes to serial connect script

--- connect-to-serial-com.sh
+++ connect-to-serial-com.sh
@@ -10,4 +10,20 @@
 # https://protectli.com/kb/com-port-tutorial/
 # https://docs.netgate.com/pfsense/en/latest/hardware/connect-to-console.html
 
-screen /dev/ttyUSB0 115200
+screen -S s1 /dev/ttyUSB0 115200
+
+##
+# From another terminal, send Function key presses
+# https://www.gnu.org/software/screen/manual/screen.html
+
+# F1
+# screen -S s1 -X stuff "$(printf '\eOP')"
+
+# F2
+# screen -S s1 -X stuff "$(printf '\eOQ')"
+
+# F11
+# # screen -S s1 -X stuff "$(printf '\e[23~')"
+
+# F12
+# screen -S s1 -X stuff "$(printf '\e[24~')"
