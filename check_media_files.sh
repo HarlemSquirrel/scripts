86f25135b35c0ed4819bfc20d897e8f919a991b2
Kevin McCormack <kevin@mccormack.tech>
2026-04-16 14:06:14 -0400

    Save updates

--- check_media_files.sh
+++ check_media_files.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-find $1 \( -name "*.flac" -o -name "*.mp3" -o -name "*.m4a" \) -type f | while read -r filename; do
+find "$1" \( -name "*.flac" -o -name "*.mp3" -o -name "*.m4a" \) -type f | while read -r filename; do
 # for filename in $1; do
   # ffmpeg -v error -i "$line" -f null - > /dev/null 2>&1 || printf " FAILED - $line \n"
-  if ffprobe -loglevel error "$filename" ; then
+  # if ffprobe -loglevel error "$filename" ; then
+  echo "$filename"
+  if ffmpeg -i "$filename" -f null -loglevel error -; then
     printf '.'
   else
     printf "\n FAILED - $filename \n"
   fi
 done
 
-printf "done!\n"
+printf "\n\nDone!\n"
