dac080c1ca26a263996cd656b14a45f54eac2724
Kevin McCormack <kevin@mccormack.tech>
2026-05-25 13:10:39 -0400

    Update docker deb update script

--- docker_deb_update.sh
+++ docker_deb_update.sh
@@ -17,11 +17,18 @@ docker system prune -f
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 
-# Update containers
-docker compose pull
+if [ -f ~/portainer-compose.yaml ]; then
+  echo "Updating Portainer container..."
+  docker compose -f portainer-compose.yaml pull
+  docker compose -f portainer-compose.yaml up -d --remove-orphans --force-recreate
+else
+  echo "Updating containers..."
+  # Update containers
+  docker compose pull
 
-# So containers will come up after reboot automatically
-docker compose up -d --remove-orphans --force-recreate
+  # So containers will come up after reboot automatically
+  docker compose up -d --remove-orphans --force-recreate
+fi
 
 # Reboot
 sudo reboot
