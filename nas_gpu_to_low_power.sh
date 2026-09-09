285e833a28102ef20d97ec4edf18b393ee709af6
Kevin McCormack <kevin@mccormack.tech>
2026-09-08 21:14:41 -0400

    Update nas_gpu_to_low_power.sh

--- nas_gpu_to_low_power.sh
+++ nas_gpu_to_low_power.sh
@@ -8,5 +8,8 @@ get_power_usage
 
 echo low | sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level
 
+echo "Waiting for power usage to stabilize..."
+sleep 3
+
 echo "Power usage is now"
 get_power_usage
