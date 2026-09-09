ff930b0a098f8b7ae0f8ad52cd8eac6a1a435667
Kevin McCormack <kevin@mccormack.tech>
2026-09-08 21:13:20 -0400

    Add nas_gpu_to_low_power.sh

---
+++ nas_gpu_to_low_power.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function get_power_usage() {
+    cat /sys/class/drm/card0/device/hwmon/hwmon2/power1_input | awk '{print $1/1000000} watts'
+}
+echo "Power usage is"
+get_power_usage
+
+echo low | sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level
+
+echo "Power usage is now"
+get_power_usage
